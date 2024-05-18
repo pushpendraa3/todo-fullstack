6508c28718cba4af5fbd26c39eba654d280e3d51
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-05-18 14:47:06 +0530

    basic express app and api signatures

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+
+
+app.use(express.json());
+
+/**
+ * @param createTodo 
+ *      - 
+ */
+app.post()
+
+/**
+ * @param getAllTodos 
+ *      - 
+ */
+app.get()
+
+/**
+ * @param updateTodo 
+ *      - 
+ */
+app.postt()
+
+/**
+ * @param deleteTodo 
+ *      - 
+ */
+app.delete()
+
+app.listen(3000, () => console.log("server running on port 3000"))
