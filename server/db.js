3520030ea57893a74a9f9a415fbf1ee7d63702ee
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-05-18 16:23:15 +0530

    add todo model and schema

---
+++ server/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/todo")
+.then(() => console.log("DB Connected!!"))
+.catch((err) => console.log(`problem connecting database! ${err}`))
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean,
+
+});
+
+const todo = mongoose.model("todo", todoSchema);
+
+export { todo };
